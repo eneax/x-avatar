d8f805c344b3edbbecb84a6a0286d14d770f2a15
Enea Xharja <19818937+eneax@users.noreply.github.com>
2023-01-14 16:32:51 +0100

    feat: create generate backend api

---
+++ src/pages/api/generate.ts
@@ -0,0 +1,35 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+const generateAction = async (req: NextApiRequest, res: NextApiResponse) => {
+  console.log("Received request");
+
+  const input = JSON.parse(req.body).input;
+
+  const response = await fetch(
+    `https://api-inference.huggingface.co/models/${process.env.HUGGING_FACE_USERNAME}/${process.env.HUGGING_FACE_MODEL_NAME}`,
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.HUGGING_FACE_AUTH_KEY}`,
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({
+        inputs: input,
+      }),
+    }
+  );
+
+  if (response.ok) {
+    const buffer = await response.arrayBuffer();
+    res.status(200).json({ image: buffer });
+  } else if (response.status === 503) {
+    const json = await response.json();
+    res.status(503).json(json);
+  } else {
+    const json = await response.json();
+    console.error(json);
+    res.status(response.status).json({ error: response.statusText });
+  }
+};
+
+export default generateAction;
