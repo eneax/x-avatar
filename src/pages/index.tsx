77e1a4bde3357597feb1cccd8a23bfb4240b779a
Enea Xharja <19818937+eneax@users.noreply.github.com>
2023-01-15 16:24:32 +0100

    feat: move generate button inside textarea

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -97,34 +97,36 @@ const Home: NextPage = () => {
       <div className="flex flex-col justify-center items-left border-gray-700 pt-8 pb-16">
         <div className="flex flex-col text-left min-h-screen">
           <div className="w-full mb-8">
-            <textarea
-              name="magic-prompt"
-              id="magic-prompt"
-              cols={30}
-              rows={5}
-              disabled={isGenerating}
-              className="block w-full text-lg px-4 py-2 bg-gray-800 text-primary-50 border rounded-2xl border-gray-900 focus:outline-none focus:ring-2 focus:ring-primary-100 focus:border-transparent resize-none"
-              value={input}
-              onChange={onUserChangedText}
-            />
-
-            <div className="flex flex-col items-start border-gray-700 mt-6">
-              <button
-                className="no-underline font-semibold px-4 py-2.5 rounded-2xl text-black bg-primary-400 hover:bg-primary-400/95 transition duration-300"
-                onClick={generateAction}
+            <div className="inline-block relative w-full">
+              <textarea
+                name="magic-prompt"
+                id="magic-prompt"
+                cols={30}
+                rows={3}
                 disabled={isGenerating}
-              >
-                {isGenerating ? (
-                  <span>
-                    <Spinner
-                      spinnerColor="text-primary-50"
-                      className="inline w-4 h-4 text-white animate-spin"
-                    />
-                  </span>
-                ) : (
-                  <span>Generate</span>
-                )}
-              </button>
+                className="block w-full text-lg px-4 pt-2 pb-14 bg-gray-800 text-primary-50 border rounded-2xl border-gray-900 focus:outline-none focus:ring-2 focus:ring-primary-100 focus:border-transparent resize-none"
+                value={input}
+                onChange={onUserChangedText}
+              />
+
+              <div className="absolute bottom-4 right-4 text-right">
+                <button
+                  className="no-underline font-semibold px-4 py-2.5 rounded-2xl text-black bg-primary-400 hover:bg-primary-400/95 transition duration-300"
+                  onClick={generateAction}
+                  disabled={isGenerating}
+                >
+                  {isGenerating ? (
+                    <span>
+                      <Spinner
+                        spinnerColor="text-primary-50"
+                        className="inline w-4 h-4 text-white animate-spin"
+                      />
+                    </span>
+                  ) : (
+                    <span>Generate</span>
+                  )}
+                </button>
+              </div>
             </div>
 
             {image && (
